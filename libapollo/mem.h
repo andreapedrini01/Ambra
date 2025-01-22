9684f25422fa11a696a4e638e6f3a1329e28672b
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2025-01-22 10:57:40 +0100

    Optimized COPY macro

--- libapollo/mem.h
+++ libapollo/mem.h
@@ -1,25 +1,32 @@
 #ifndef _LIBAPOLLO_MEM_H
 #define _LIBAPOLLO_MEM_H
 
-#define WORD 4
+#include "am_util.h"
 
-#define _nv __align(16) __attribute__((section("_nv")))
+#define WORDS 4
 
-#define WRITE_MRAM(array_ptr, start_address, size)                                			    \
+#define __nv __align(16) __attribute__((section("_nv")))
+
+// SRAM array to store values that will be copied in MRAM
+static uint32_t buffer[1];
+
+// y is the source value you want to copy in x, that is the destination in MRAM
+#define COPY_VALUE(x, y)                                																\
     do                                                                         	 				\
     {                                                                           				\
-        if (((uint32_t)(start_address) % 16) != 0)                              				\
+        if (((uintptr_t)(&x) % 16) != 0)                             										\
         {                                                                       				\
-            am_util_stdio_printf("The starting address is not alligned to 16 byte.\n");         \
+            am_util_stdio_printf("The starting address is not alligned to 16 byte.\n"); \
         }                                                                       				\
         else                                                                    				\
         {                                                                       				\
+						buffer[0] = (uint32_t)(y);																									\
             returnCode = am_hal_mram_main_program(AM_HAL_MRAM_PROGRAM_KEY,  				    \
-                                                      (array_ptr),                  		    \
-                                                      (start_address),          				\
-                                                      (size));                  				\
+                                                      (buffer),                  				\
+                                                      (&x),          										\
+                                                      (WORDS));                  				\
             am_util_stdio_printf("MRAM program returnCode = %d\n", returnCode); 				\
         }                                                                       				\
     } while (0)
-
+		
 #endif // _LIBAPOLLO_MEM_H
