11091f47858847b25f09944d21e8ecf20b9bdf78
Andrea <99681677+andreapedrini01@users.noreply.github.com>
2025-03-20 12:43:19 +0100

    Crit vars listed

---
+++ libapollo/critical_variables.h
@@ -0,0 +1,36 @@
+#ifndef CRITICAL_VARIABLES_H
+#define CRITICAL_VARIABLES_H
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define ALIGN16 __attribute__((aligned(16)))
+
+typedef struct
+{
+	uint32_t value ALIGN16;
+} AlignedVar;
+
+/**
+ * @brief Structure containing power-failure-resilient variables.
+ *
+ * The `CritVar` struct holds all critical variables that must be retained 
+ * across power failures. These variables are properly aligned to ensure 
+ * atomic access and compatibility with MRAM storage.
+ * Example:
+ * - `x` and `y`: Aligned variables for critical computations.
+ * - `array[3]`: A three-element array, explicitly aligned to 16 bytes.
+ *
+ * Any new variables required for executing and storing task calculations 
+ * should be added to this struct, ensuring they adhere to the same 
+ * alignment constraints for power-failure resilience.
+ */
+typedef struct
+{
+	//These are for example, change them on what you need
+		AlignedVar x;
+		AlignedVar y;
+		AlignedVar array[3] ALIGN16;
+} CritVar;
+
+#endif // CRITICAL_VARIABLES_H
